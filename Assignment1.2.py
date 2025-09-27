7b2bfcaafd4f62f6217c60dddc6edbca0d0756a6
vs502382-cmd <vs502382@gmail.com>
2025-09-27 20:44:13 +0530

    Add files via upload

---
+++ Assignment1.2.py
@@ -0,0 +1,10 @@
+
+a=input('Enter your first name: ')
+b=input('Enter your last name:  ')
+
+print('Hello,')
+c=a +b
+print(c)
+
+print('Welcome to the Python program.')
+
---
+++ assignment1 TASK1.py
@@ -0,0 +1,13 @@
+a=6
+b=10
+c=a+b
+print(c)
+
+d=b-a
+print(d)
+
+e=a*b
+print(e)
+
+f=a/b
+print(f)
